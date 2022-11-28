ed05af50eeafecf378109e08e6a4e6224f82a13c
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2022-11-28 12:15:05 +0530

    initialized app

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar />
+      <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+        <Text>Arogya360</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
