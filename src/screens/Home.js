eae8f2c64dcd79829e96a6379c559dd1fd8ab0a2
sakshichavre <sakshi.chavre@dwebbox.com>
2022-12-01 22:53:19 +0530

    [ADD] font integration

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,21 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-
+import {FONT} from '../constants/contants';
+import Location from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Location name="location" size={21} color={'red'} />
+      <FontAwesome name="bookmark" size={24} color="black" />
+      <Text style={{color: 'black', fontSize: 16}}>Home</Text>
+      <Text style={{...FONT.header}}>Home</Text>
     </View>
   );
 };
