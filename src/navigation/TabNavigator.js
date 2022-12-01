aab1989cc1a8ff7a55d2127401eaf7a5c5676087
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2022-12-02 00:40:48 +0530

    bottom tab floating layer

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {ROUTES} from '../constants/contants';
+import {DIMENSIONS, ROUTES} from '../constants/contants';
 import Home from '../screens/Home';
 import Chat from '../screens/Chat';
 import Article from '../screens/Article';
+import CustomTabBar from '../components/CustomTabBar';
 
 const Tab = createMaterialBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      // tabBar={props => <CustomTabBar {...props} />}
+      // screenOptions={props => {
+      //   return {
+      //     tabBarHideOnKeyboard: true,
+      //   };
+      // }}
+      // tabBarOptions={{
+      //   borderTopLeftRadius: 40,
+      //   borderTopRightRadius: 40,
+      //   marginTop: -40,
+      // }}
+      barStyle={{
+        backgroundColor: '#fff',
+        width: DIMENSIONS.width - 60,
+        alignSelf: 'center',
+        bottom: 20,
+        elevation: 20,
+        shadowOffset: {height: 10},
+        // shadowColor: '#00f',
+        borderRadius: 24,
+        // height: 70,
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
+        padding: 10,
+      }}>
       <Tab.Screen name={ROUTES.home} component={Home} />
       <Tab.Screen name={ROUTES.chat} component={Chat} />
       <Tab.Screen name={ROUTES.article} component={Article} />
