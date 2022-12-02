550374cc3eb6ac178ee81e1de0f9712b776c6882
sakshichavre20 <chavresakshi@gmail.com>
2022-12-02 13:42:21 +0530

    [ADD] custom header , custom buttons, Container

---
+++ src/components/Container.js
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {COLORS} from '../constants/contants';
+
+const Container = props => {
+  return (
+    <View style={{...styles.container, ...props.style}}>{props.children}</View>
+  );
+};
+
+export default Container;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: COLORS.background,
+    flex: 1,
+  },
+});
--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -6,6 +6,7 @@ import {ROUTES} from '../constants/contants';
 import Appointment from '../screens/Appointment';
 import Profile from '../screens/Profile';
 import Settings from '../screens/Settings';
+import CustomHeader from '../components/CustomHeader';
 
 const Drawer = createDrawerNavigator();
 
@@ -13,9 +14,36 @@ const DrawerNavigator = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name={ROUTES.tabNav} component={TabNavigator} />
-      <Drawer.Screen name={ROUTES.appointment} component={Appointment} />
-      <Drawer.Screen name={ROUTES.profile} component={Profile} />
-      <Drawer.Screen name={ROUTES.settings} component={Settings} />
+      <Drawer.Screen
+        name={ROUTES.appointment}
+        component={Appointment}
+        options={{
+          headerShown: true,
+          header: stackHeaderProps => (
+            <CustomHeader {...stackHeaderProps} title="Book Appointment" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name={ROUTES.profile}
+        component={Profile}
+        options={{
+          headerShown: true,
+          header: stackHeaderProps => (
+            <CustomHeader {...stackHeaderProps} title="Profile" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name={ROUTES.settings}
+        component={Settings}
+        options={{
+          headerShown: true,
+          header: stackHeaderProps => (
+            <CustomHeader {...stackHeaderProps} title="Settings" />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 };
