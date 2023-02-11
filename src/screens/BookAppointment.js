4b87f8cfe4474549ff28e2e1e85048c75111c729
sakshichavre20 <chavresakshi@gmail.com>
2023-02-11 12:24:11 +0530

    [add] bookappoitment

---
+++ src/screens/BookAppointment.js
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Container from '../components/Container';
+
+const BookAppointment = ({navigation}) => {
+  return (
+    <Container>
+      <Text>BookAppoitment</Text>
+    </Container>
+  );
+};
+
+export default BookAppointment;
+
+const styles = StyleSheet.create({});
