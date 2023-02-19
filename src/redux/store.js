e0b41831188dbb0df2a750aa87a00f70d1f85188
sakshichavre20 <chavresakshi@gmail.com>
2023-02-19 11:52:09 +0530

    [ADD] is authenticated error solved

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,20 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import thunk from 'redux-thunk';
-
-import reducer from './reducer';
-
-export default createStore(reducer, applyMiddleware(thunk));
-
-// import {applyMiddleware, createStore} from 'redux';
-// import {persistStore, persistReducer} from 'redux-persist';
-// import reducer from '../Redux/reducer';
-// import {composeWithDevTools} from 'redux-devtools-extension';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import {createStore, applyMiddleware, combineReducers} from 'redux';
 // import thunk from 'redux-thunk';
 
-// const persistConfig = {
-//   key: 'root',
-//   storage: AsyncStorage,
-// };
-// const persistedReducer = persistReducer(persistConfig, reducer);
+// export default createStore(reducer, applyMiddleware(thunk));
 
-// export const store = createStore(persistedReducer, applyMiddleware(thunk));
-// export const persistor = persistStore(store);
+import {applyMiddleware, createStore} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import reducer from './reducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
